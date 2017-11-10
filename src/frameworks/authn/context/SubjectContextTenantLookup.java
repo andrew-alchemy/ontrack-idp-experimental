32f96735fde66829fd9f2366d73fa98485c900fd
Andrew Tomlinson <andrew.tomlinson@alchemysystems.com>
2017-11-10 12:54:37 -0500

    add some comments

--- src/frameworks/authn/context/SubjectContextTenantLookup.java
+++ src/frameworks/authn/context/SubjectContextTenantLookup.java
@@ -13,14 +13,33 @@ import com.google.common.base.Function;
 import net.shibboleth.idp.attribute.IdPAttributeValue;
 import net.shibboleth.idp.authn.context.SubjectContext;
 
+/**
+ * This is used to apply an "attribute getter" function to all the principals in a Subject held in a SubjectContext
+ * 
+ * In our SSO controller we define a Subject and all its principals based on the current (Oauth) authentication
+ * This is the tunneled through to the preauth flow where they can be used by code like this that extracts the data and 
+ * uses it to expose SAML attributes or control behaviour e.g. in an activationCondition predicate   
+ * 
+ *  See fw-attribute-resoucres.xml
+ * 
+ * @author Andrew
+ *
+ */
 public class SubjectContextTenantLookup implements ContextDataLookupFunction<SubjectContext, String> {
 	
 	private Function<Principal, List<IdPAttributeValue<?>>> attributesValuesFunction; 
 
+	/**
+	 * this will be the injected function bean that extracts attribute values from a Principal
+	 * @param function
+	 */
 	public void setAttributeValuesFunction( final Function<Principal, List<IdPAttributeValue<?>>> function) {
 		 this.attributesValuesFunction = function;
 	}
 
+	/**
+	 * this just iterates over all principals in the Subject(s) and gets the attribute value(s) 
+	 */
 	public String apply(final SubjectContext input) {
 		List<IdPAttributeValue<?>> results = new ArrayList<IdPAttributeValue<?>>(1);
 		for (Subject subject : input.getSubjects()) {
